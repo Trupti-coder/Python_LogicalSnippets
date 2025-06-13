ffd80617a4c4d71cc472b2299bca23434baf0e00
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-13 10:54:02 -0400

    Update

--- rotate_array.py
+++ rotate_array.py
@@ -2,3 +2,10 @@ def rotate_array(array, k):
     n = len(array)
     k = k % n  # handle if k > n
     result = [0] * n
+
+    for i in range(n):
+          
+          result[(i + k) % n] = array[i]
+
+    # copy back to original array if needed, or just return result
+    return result

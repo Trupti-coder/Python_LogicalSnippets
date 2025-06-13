29eda3d022ec25612c57607c6cc3042a33a468f6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-12 22:00:38 -0400

    Update

--- reverse_array.py
+++ reverse_array.py
@@ -8,3 +8,9 @@ def reverse_array(array):
         end -= 1
 
     return array
+
+
+# Example usage
+original_array = [1, 2, 3, 4, 5, 6]
+new_array = reverse_array(original_array)
+print(new_array)  # Output: [6, 5, 4, 3, 2, 1]

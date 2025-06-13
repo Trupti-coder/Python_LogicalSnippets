9c875cc756efc41eb126bb41e6002fcb3fcbcf90
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-12 22:00:15 -0400

    Update

--- reverse_array.py
+++ reverse_array.py
@@ -1,3 +1,10 @@
 def reverse_array(array):
     start = 0
     end = len(array) - 1
+    while start < end:
+        # swap elements at start and end
+        array[start], array[end] = array[end], array[start]
+        start += 1
+        end -= 1
+
+    return array

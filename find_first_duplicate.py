2018da1b768a280785962d7ba17dc1ed78851661
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-13 11:05:09 -0400

    Update

--- find_first_duplicate.py
+++ find_first_duplicate.py
@@ -5,3 +5,7 @@ def find_first_duplicate(arr):
                  return num
         seen.add(num)
     return -1  # no duplicates
+
+# Example usage
+arr = [1, 2, 3, 4, 5, 4]
+print(find_first_duplicate(arr))  # Output: 4

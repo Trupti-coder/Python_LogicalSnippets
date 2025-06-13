382e069d49e2711769b1c0e567d98b92015ad860
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-13 11:00:38 -0400

    Update

--- sort_array.py
+++ sort_array.py
@@ -3,3 +3,11 @@ def sort_array(arr):
     for i in range(n - 1):
         for j in range(n - 1 - i):
             if arr[j] > arr[j + 1]:
+
+                 # swap elements
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+# Example usage
+array = [1, 4, 6, 8, 3, 4]
+print(sort_array(array))  # Output: [1, 3, 4, 4, 6, 8]

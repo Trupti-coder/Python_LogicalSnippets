1a73a373af5856e51ce70668b0bd1dc5b4eab10e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-14 15:57:41 -0400

    Update

--- array_to_object.py
+++ array_to_object.py
@@ -5,3 +5,7 @@ def array_to_object(arr):
             return num
         seen[num]=True
         return -1
+    
+# Example usage
+arr = [1, 2, 3, 4, 5, 4]
+print(array_to_object(arr))  # Output: 4

d23831ec12e566347bc4cd3678819b045c850f9e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-14 16:01:02 -0400

    update

--- capitlize_even_odd_char.py
+++ capitlize_even_odd_char.py
@@ -2,4 +2,8 @@ def capitlize_even_odd_char(char):
     capitlized_arr=[]
 
     for word in arr:
-        
+        capitalized_word = ''
+        for i in range(len(word)):
+            char = word[i]
+            if i % 2 == 0:
+

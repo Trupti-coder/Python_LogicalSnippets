ed0618deaad5bbe781bebfad94b8086b05dcfffa
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-15 21:32:25 -0400

    Update

--- capitlize_first_char.py
+++ capitlize_first_char.py
@@ -1,2 +1,10 @@
 def capitalize_first_char(arr):
     uppercased_arr = []
+
+    
+    for word in arr:
+        if len(word) > 0:
+            # Capitalize first character manually, leave the rest unchanged
+            first_char = word[0].upper()
+            capitalized_word = first_char + word[1:]
+        else:

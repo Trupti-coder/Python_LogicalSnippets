80be2cbacfb650d29dee056b490ff48da6f86d67
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-15 21:41:12 -0400

    Update

--- array_chunk.py
+++ array_chunk.py
@@ -6,3 +6,14 @@ def array_chunk(arr, k):
         if len(chunk) == k:
             result.append(chunk)
             chunk = []
+
+    if len(chunk) > 0:
+        result.append(chunk)
+        
+    return result
+
+# Example usage
+array = [1, 2, 3, 4, 5, 6, 6, 7, 88, 9, 90, 8, 5]
+k = 2
+result = array_chunk(array, k)
+print(result)

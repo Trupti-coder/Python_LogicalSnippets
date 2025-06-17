7bcf5fd814cae4b79be15fd2cf9738afbdd41919
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-16 21:44:51 -0400

    changed

--- findPair.py
+++ findPair.py
@@ -1,10 +1,11 @@
 def find_pairs(arr, target):
     pairs = []
-     for i in range(len(arr)):
+    for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
             if arr[i] + arr[j] == target:
                 pairs.append([arr[i], arr[j]])
     return pairs
+
 # Example usage
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 target = 18

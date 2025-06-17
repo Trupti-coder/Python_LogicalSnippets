9e3ec22538ff3d37a1f5f8f54d4d46aedea6109f
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-16 21:40:45 -0400

    Update

--- find_max_num.py
+++ find_max_num.py
@@ -4,3 +4,8 @@ def find_max_num(arr):
         if arr[i] > max_num:
             max_num = arr[i]
     return max_num
+
+
+# Example usage
+arr = [1, 2, 3, 4, 5, 6]
+print(find_max_num(arr))  # Output: 6

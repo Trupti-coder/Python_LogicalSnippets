5b8eea683672ec45db3665a271df6ac84be4d41b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-17 20:44:48 -0400

    Update

--- findUniqueElement.py
+++ findUniqueElement.py
@@ -7,4 +7,12 @@ function findUniqueElements(arr) {
     let num1 = 0, num2 = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] & setBit) {
+             num1 ^= arr[i];
+        } else {
+            num2 ^= arr[i];
+        }
+    }
+
+    return [num1, num2];
+}
 

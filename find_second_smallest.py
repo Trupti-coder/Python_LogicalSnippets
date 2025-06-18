d765a20b32fec9bbb06dc5dd5f1a9696ef8d94df
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-17 20:36:15 -0400

    Update

--- find_second_smallest.py
+++ find_second_smallest.py
@@ -1,3 +1,7 @@
 def find_second_smallest(arr):
     if len(arr) < 2:
         return False
+    smallest = float('inf')
+    second_smallest = float('inf')
+     for num in arr:
+        if num < smallest:

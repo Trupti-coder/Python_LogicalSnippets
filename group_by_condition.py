cc398a4426824c57297873b0a4350eafc9f79f9a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-18 22:54:22 -0400

    Update

--- group_by_condition.py
+++ group_by_condition.py
@@ -9,3 +9,8 @@ def group_by_condition(arr, condition):
             group2.append(item)
            
     return [group1, group2]
+# Example usage: Group even and odd numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+grouped_numbers = group_by_condition(numbers, lambda num: num % 2 == 0)
+
+print(grouped_numbers)  # Output: [[2, 4, 6, 8, 10], [1, 3, 5, 7, 9]]

67e4769c9597dc6b19786d32df8b301727178fb6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-18 22:53:26 -0400

    update

--- group_by_condition.py
+++ group_by_condition.py
@@ -1,3 +1,9 @@
 def group_by_condition(arr, condition):
     group1 = []
     group2 = []
+    
+    for item in arr:
+        if condition(item):
+            group1.append(item)
+        else:
+            group2.append(item)

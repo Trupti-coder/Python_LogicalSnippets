e5898ff04491ad02eb5521a980168eba2b8e45b0
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-18 22:51:01 -0400

    update

--- generate_series.py
+++ generate_series.py
@@ -1,2 +1,6 @@
 def generate_series():
     series = []
+     for i in range(1, 11):
+        series.append(i)       # Add the number
+        series.append(i * 2)   # Add the number multiplied by 2
+    return series

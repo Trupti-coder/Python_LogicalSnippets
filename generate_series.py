e8fe5181617fcdc2d62433b4f484d25d0bb03772
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-18 22:51:44 -0400

    Update

--- generate_series.py
+++ generate_series.py
@@ -1,9 +1,10 @@
 def generate_series():
     series = []
-     for i in range(1, 11):
+    for i in range(1, 11):
         series.append(i)       # Add the number
         series.append(i * 2)   # Add the number multiplied by 2
     return series
+
 # Example usage
 result = generate_series()
 print(result)  # Output: [1, 2, 2, 4, 3, 6, 4, 8, ..., 10, 20]

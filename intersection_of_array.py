ed0dbbee405fe8f7e2d8295bdef26b64e74ad3c7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-19 22:48:16 -0400

    update

--- intersection_of_array.py
+++ intersection_of_array.py
@@ -1,3 +1,9 @@
 def intersection_of_array(arr1, arr2):
     result = []
     arr2_copy = arr2.copy()  # To avoid modifying the original arr2
+     for item1 in arr1:
+        for i in range(len(arr2_copy)):
+            if item1 == arr2_copy[i]:
+                result.append(item1)
+                arr2_copy[i] = None  # Mark as used
+                break

7234ce7750cad0b2a6ff294c8ddedf8aee488122
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-19 22:51:54 -0400

    update

--- find_major_element.py
+++ find_major_element.py
@@ -3,3 +3,6 @@ def find_major_element(arr):
     majority_element = None
      for i in range(len(arr)):
         count = 0
+        for j in range(len(arr)):
+            if arr[i] == arr[j]:
+                count += 1

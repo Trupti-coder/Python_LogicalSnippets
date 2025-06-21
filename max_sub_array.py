a8a6cd60746a95eb13b84ae2a3236a7265a37ef7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-20 22:04:45 -0400

    Update

--- max_sub_array.py
+++ max_sub_array.py
@@ -3,3 +3,7 @@ def max_sub_array(arr):
         return 0
  max_sum = arr[0]
     current_sum = arr[0]
+ for i in range(1, len(arr)):
+        if arr[i] > current_sum + arr[i]:
+            current_sum = arr[i]  # Start a new subarray
+        else:

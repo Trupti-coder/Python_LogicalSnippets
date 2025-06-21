f435538900a58cb03b3932eec4caca788c582654
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-20 21:52:25 -0400

    Update

--- max_product.py
+++ max_product.py
@@ -2,3 +2,7 @@ def max_product(arr):
  max_prod = float('-inf')
     for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
+            product = arr[i] * arr[j]
+            if product > max_prod:
+                max_prod = product
+    return max_prod

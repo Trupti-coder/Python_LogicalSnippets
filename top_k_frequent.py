42733870b03e1bcd69daf9b69ca8a302ff5c1ca8
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-22 14:49:50 -0400

    update

--- top_k_frequent.py
+++ top_k_frequent.py
@@ -5,3 +5,10 @@ def top_k_frequent(nums, k):
     for num in nums:
         frequency_map[num] = frequency_map.get(num, 0) + 1
 
+         # Step 2: Create buckets
+    buckets = [[] for _ in range(len(nums) + 1)]
+    for num, freq in frequency_map.items():
+        buckets[freq].append(num)
+
+
+

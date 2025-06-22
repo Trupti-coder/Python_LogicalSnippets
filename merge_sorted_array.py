67f0d09a0fb9eb3a8087c7cf33b49236b45dc9bb
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-22 14:43:26 -0400

    Update

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,3 +1,9 @@
 def merge_sorted_arrays(arr1, arr2):
     merged_array = []
     i = j = 0
+
+while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            merged_array.append(arr1[i])
+            i += 1
+        else:

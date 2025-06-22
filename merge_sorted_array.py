71990c1dc8c002af1203f0eda0a1a1e9e76fb73a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-22 14:44:43 -0400

    Update

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -12,6 +12,13 @@ while i < len(arr1) and j < len(arr2):
         while i < len(arr1):
         merged_array.append(arr1[i])
         i += 1
+         while j < len(arr2):
+        merged_array.append(arr2[j])
+        j += 1
+
+    return merged_array
+
+
 
 
 

f4b673df35afcc79866aff6eadfca1b3de2b297b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-23 22:37:09 -0400

    Update

--- find_union.py
+++ find_union.py
@@ -4,3 +4,7 @@ def find_union(arr1, arr2):
     for item in arr1:
         if item not in result:
             result.append(item)
+
+             for item in arr2:
+        if item not in result:
+            result.append(item)

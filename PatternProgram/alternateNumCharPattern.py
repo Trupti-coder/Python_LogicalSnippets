0492112c8ab27d2522d632f04ac20f4140eab8a3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-23 22:41:15 -0400

    Update

--- PatternProgram/alternateNumCharPattern.py
+++ PatternProgram/alternateNumCharPattern.py
@@ -1,3 +1,10 @@
 n = 5
 char_code = 97  # ASCII for 'a'
 num = 1
+for i in range(n):
+    line = ""
+    for j in range(i + 1):
+        if j % 2 == 0:
+            line += str(num)
+            num += 1
+        else:

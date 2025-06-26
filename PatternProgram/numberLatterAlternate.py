1a9482662b04f0d0d22f9653bc9ac67dd3d32c09
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-25 21:50:00 -0400

    Update

--- PatternProgram/numberLatterAlternate.py
+++ PatternProgram/numberLatterAlternate.py
@@ -3,10 +3,11 @@ num = 1
 char_code = 97
 for i in range(n):
     line = ''
-     for j in range(i + 1):
+    for j in range(i + 1):
         if j % 2 == 0:
             line += str(num)
             num += 1
-             line += chr(char_code)
+        else:
+            line += chr(char_code)
             char_code += 1
     print(line)

7414de7162e4340c824f93fb1308e0dffa29b9a0
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-26 22:48:57 -0400

    Update

--- PatternProgram/spiralNumberMatrix.py
+++ PatternProgram/spiralNumberMatrix.py
@@ -3,17 +3,17 @@ def spiral_matrix(n):
     num = 1
     top, left = 0, 0
     bottom, right = n - 1, n - 1
-     while top <= bottom and left <= right:
+
+    while top <= bottom and left <= right:
         for i in range(left, right + 1):
             matrix[top][i] = num
             num += 1
         top += 1
-         for i in range(top, bottom + 1):
+        for i in range(top, bottom + 1):
             matrix[i][right] = num
             num += 1
         right -= 1
-
-         for i in range(right, left - 1, -1):
+        for i in range(right, left - 1, -1):
             matrix[bottom][i] = num
             num += 1
         bottom -= 1
@@ -22,7 +22,7 @@ def spiral_matrix(n):
             num += 1
         left += 1
 
-          for row in matrix:
+    for row in matrix:
         print(" ".join(f"{val:2}" for val in row))
 
 spiral_matrix(5)

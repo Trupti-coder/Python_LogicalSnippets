b3e4793d29c77c94ae19c26fce3c07f2cb20d3b0
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-26 22:45:54 -0400

    update

--- PatternProgram/pascalTriangle.py
+++ PatternProgram/pascalTriangle.py
@@ -3,7 +3,7 @@ def pascal_triangle(n):
         val = 1
         print(' ' * (n - i), end='')
         for j in range(i + 1):
-                        print(val, end=' ')
+            print(val, end=' ')
             val = val * (i - j) // (j + 1)
         print()
 

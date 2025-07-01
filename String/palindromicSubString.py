2bb4f15bcf31e040909a0116b3f80d0a8b59333a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-30 22:20:02 -0400

    Update

--- String/palindromicSubString.py
+++ String/palindromicSubString.py
@@ -3,7 +3,7 @@ def longest_palindrome(s):
     for i in range(len(s)):
         for j in range(i, len(s)):
             sub = s[i:j+1]
-               if sub == sub[::-1] and len(sub) > len(result):
+            if sub == sub[::-1] and len(sub) > len(result):
                 result = sub
     return result
 

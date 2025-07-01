da5ea410b5f653c2f9ffc5dca6ada21ea9b7f552
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-30 22:17:26 -0400

    Update

--- String/palindromicSubString.py
+++ String/palindromicSubString.py
@@ -3,3 +3,8 @@ def longest_palindrome(s):
     for i in range(len(s)):
         for j in range(i, len(s)):
             sub = s[i:j+1]
+               if sub == sub[::-1] and len(sub) > len(result):
+                result = sub
+    return result
+
+print(longest_palindrome("babad"))  # Output: "bab" or "aba"

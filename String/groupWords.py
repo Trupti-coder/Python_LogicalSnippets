f9e358d4365ca6bd94e7ca5db25b032178d8475a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-30 22:16:12 -0400

    Update

--- String/groupWords.py
+++ String/groupWords.py
@@ -2,3 +2,5 @@ words = ["eat", "tea", "ate", "bat", "tab"]
 from collections import defaultdict
 groups = defaultdict(list)
 for word in words:
+        groups[''.join(sorted(word))].append(word)
+print(list(groups.values()))

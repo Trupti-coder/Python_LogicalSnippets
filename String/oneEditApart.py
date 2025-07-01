7e8bb3d59043bbf4e78331acd6265ef27ab72429
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-06-30 22:18:39 -0400

    Update

--- String/oneEditApart.py
+++ String/oneEditApart.py
@@ -4,3 +4,7 @@ def one_edit_apart(s1, s2):
     if len(s1) > len(s2):
         s1, s2 = s2, s1
     i = j = edits = 0
+     while i < len(s1) and j < len(s2):
+        if s1[i] != s2[j]:
+            if edits == 1:
+                return False

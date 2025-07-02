290f499840226535a0df89c8306c6cc2a5484264
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-07-01 21:56:14 -0400

    Update

--- String/balancedParentheses.py
+++ String/balancedParentheses.py
@@ -2,12 +2,11 @@ def is_balanced(s):
     stack = []
     pairs = {')': '(', ']': '[', '}': '{'}
     for ch in s:
-         if ch in "([{":
+        if ch in "([{":
             stack.append(ch)
         elif ch in ")]}":
             if not stack or stack.pop() != pairs[ch]:
                 return False
-    
-     return not stack
+    return not stack
 
 print(is_balanced("{[()]}"))  # Output: True

1b7ac1f2ee873d395bbdffd6bc71262c145d2e48
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-07-04 21:33:12 -0400

    Update

--- String/withoutRepeatingChar.py
+++ String/withoutRepeatingChar.py
@@ -2,3 +2,6 @@ s = "this challenge is interesting"
 words = s.split()
 max_len = 0
 for word in words:
+       if len(set(word)) == len(word):
+        max_len = max(max_len, len(word))
+print(max_len)  # Output: 11 (interesting)
